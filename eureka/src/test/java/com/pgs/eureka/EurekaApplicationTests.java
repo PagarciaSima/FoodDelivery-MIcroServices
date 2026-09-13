587177d6a410238534bcb7e02053a76df2d20d90
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-09-13 16:01:28 +0200

    first commit

---
+++ eureka/src/test/java/com/pgs/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgs.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
